d427bceeec3140d8ac175d1f1bed09507cde41e3
Damian <damian.gawrys@gmail.com>
2022-12-06 17:06:21 +0100

    Converter F/C ready

--- script.js
+++ script.js
@@ -6,4 +6,45 @@ const changeBtn = document.querySelector('.change')
 const one = document.querySelector('.one')
 const two = document.querySelector('.two')
 
-//
+const swap = () => {
+	if (one.textContent === '°C') {
+		one.textContent = '°F'
+		two.textContent = '°C'
+        result.textContent = ''
+	} else {
+		one.textContent = '°C'
+		two.textContent = '°F'
+        result.textContent = ''
+	}
+}
+
+
+
+const convertF = () => {
+    if (one.textContent === '°C') {
+        fahrenheit = converter.value * 1.8 + 32
+		result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+        converter.value = ''
+	} else {
+        celsiuse = (converter.value - 32) / 1.8
+		result.textContent = `${converter.value}°F to ${celsiuse.toFixed(1)}°C`
+        converter.value = ''
+	}
+}
+
+const calculator = () => {
+if (converter.value !== '') {
+    convertF()
+} else {
+    result.textContent = 'Musisz podać jakąś wartość'
+}
+}
+
+const resetCell = () => {
+    converter.value = ''
+    result.textContent = ''
+}
+
+changeBtn.addEventListener('click', swap)
+convBtn.addEventListener('click', calculator)
+resetBtn.addEventListener('click', resetCell)
